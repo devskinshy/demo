b8dc2554af0662d92c4bcdd7f9abf08fd4ba97db
skinshy <yyshgk12@gmail.com>
2023-05-14 16:16:54 +0900

    temp

--- src/main/java/com/example/demo/controller/BoardAPIController.java
+++ src/main/java/com/example/demo/controller/BoardAPIController.java
@@ -3,13 +3,14 @@ package com.example.demo.controller;
 import com.example.demo.dto.BoardDTO;
 import com.example.demo.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @RequestMapping("/api/boards")
 @RestController
@@ -17,12 +18,24 @@ public class BoardAPIController {
     BoardService boardService;
 
     @Autowired
-    public BoardAPIController(BoardService  boardService) {
+    public BoardAPIController(BoardService boardService) {
         this.boardService = boardService;
     }
 
     @GetMapping("/")
     public ArrayList<BoardDTO> Index() {
+        ArrayList<BoardDTO> boards = boardService.getBoard();
+        System.out.println(boards);
+
+        return boards;
+    }
+
+    @PostMapping("/") // post /api/board/
+    public ArrayList<BoardDTO> Create(@RequestBody HashMap test) {
+
+        System.out.println("test");
+        System.out.println(test);
+
         ArrayList<BoardDTO> boards = boardService.getBoard();
         System.out.println(boards);
 
--- src/main/webapp/static/board/create.js
+++ src/main/webapp/static/board/create.js
@@ -13,11 +13,24 @@ $(function() {
     // FormData 객체를 생성합니다.
     var formData = new FormData(this);
 
+    var data = {};
+
+    for (var pair of formData.entries()) {
+      data[pair[0]] = pair[1];
+    }
+
+    var jsonData = JSON.stringify(data);
+
     $.ajax({
       method: 'post',
       url: '/api/boards/',
+      contentType : 'application/json; charset=utf-8',
+      data: jsonData,
+      processData: false,
       success: successFunction,
       error: errorFunction
     })
+
+
   })
 })
